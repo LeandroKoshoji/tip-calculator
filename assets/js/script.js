e402b81e5d56fd66bcc8764f4529436413eeadbd
Leandro Koshoji Araujo <leandrokoshoji@gmail.com>
2021-07-14 00:35:31 -0300

    replace the buttons for checkbox

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const billInput = document.querySelector('[data-js="bill_input"]')
+const customPercentageInput = document.querySelector('[data-js="custom_percentage"]')
+const peopleInput = document.querySelector('[data-js="people_input"]')
+const tipAmountContainer = document.querySelector('[data-js="tip_amount"]')
+const totalAmountContainer = document.querySelector('[data-js="total_amount"]')
+const resetButton = document.querySelector('[data-js="reset_button"]')
+
+
